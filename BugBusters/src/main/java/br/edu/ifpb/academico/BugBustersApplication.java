a35b9b8f1e1071b181b3cb8baa8717b57db619ea
Miguelito-244 <miguel.barbosa@academico.ifpb.edu.br>
2025-08-15 09:07:52 -0300

    Add files via upload

---
+++ BugBusters/src/main/java/br/edu/ifpb/academico/BugBustersApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.academico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugBustersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugBustersApplication.class, args);
+	}
+
+}
